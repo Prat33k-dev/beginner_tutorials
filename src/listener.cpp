6ad8afb1ab233a19bd732b6a15b7c0a8af998c66
prat33k-dev <bhujbalpratik97@gmail.com>
2021-11-07 14:05:12 -0500

    added log messages for all verbosity levels used by rosconsole in listener node

--- src/listener.cpp
+++ src/listener.cpp
@@ -35,11 +35,14 @@
 /**
  * This tutorial demonstrates simple receipt of messages over the ROS system.
  */
-void chatterCallback(const std_msgs::String::ConstPtr &msg) {
-  ROS_INFO("Patrick heard : [%s]", msg->data.c_str());
+void chatterCallback(const std_msgs::String::ConstPtr &msg)
+{
+  ROS_DEBUG_STREAM("Recieved Subscribed Message");
+  ROS_INFO_STREAM("Patrick heard :" << msg->data.c_str());
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line.
