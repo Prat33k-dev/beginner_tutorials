7104cd7407e5729a81d7d830dac6f53c1c97122f
prat33k-dev <bhujbalpratik97@gmail.com>
2021-11-07 14:03:31 -0500

    added log messages for all verbosity levels used by rosconsole

--- src/talker.cpp
+++ src/talker.cpp
@@ -35,7 +35,7 @@
 #include "beginner_tutorials/Str.h"
 
 //  Initialize value to a string to publish
-extern std::string pub_msg = "Hello from Patrick";
+  extern std::string pub_msg = "Hello from Patrick";
 /**
  * @brief A service Fucntion for changing the output string of the publisher.
  * @param req service request
@@ -43,18 +43,17 @@ extern std::string pub_msg = "Hello from Patrick";
  * @return returns true when string changes
  */
 bool SetString(beginner_tutorials::Str::Request &req,
-               beginner_tutorials::Str::Response &res)
-{
+               beginner_tutorials::Str::Response &res) {
   res.output = req.data;
   pub_msg = req.data;
+  ROS_DEBUG_STREAM("Initial String changed to : " << req.data);
   return true;
 }
 
 /**
  * This tutorial demonstrates simple sending of messages over the ROS sys tem.
  */
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line.
@@ -95,13 +94,34 @@ int main(int argc, char **argv)
    * buffer up before throwing some away.
    */
 
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+  ros::Publisher chatter_pub = n.advertise < std_msgs::String
+      > ("chatter", 1000);
   // Setting default frequency for 20 Hz
   int frequency = 20;
+  if (argc > 0) {
+    frequency = atoi(argv[1]);
+  }
+  // Stream information once about frequency set from argument
+  ROS_INFO_STREAM_ONCE("Set frequency= " << frequency);
+  if (frequency <= 0) {
+    // If frequency is negative or zero- Fatal
+    ROS_FATAL_STREAM("Frequency must be greater than zero !");
+    ROS_DEBUG_STREAM("Changing to default value i.e. 20 Hz");
+    frequency = 20;
+  } else if (frequency > 100) {
+    // If frequency is greater than expected- ERROR
+    ROS_ERROR_STREAM("Expected frequency is less than 100 Hz");
+    ROS_DEBUG_STREAM("Changing to default value i.e. 20 Hz");
+    frequency = 20;
+  } else if (0 < frequency && frequency < 5) {
+    // If frequency is less than 5 and greater than 0- WARN
+    ROS_WARN_STREAM("Input Frequency too low");
+    ROS_DEBUG_STREAM("Changing to default value i.e. 20 Hz");
+    frequency = 20;
+  }
   ros::Rate loop_rate(frequency);
 
-  while (ros::ok())
-  {
+  while (ros::ok()) {
     /**
      * This is a message object. You stuff it with data, and then publish it.
      */
