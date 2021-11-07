11c317863ad11b3a69be1cc4c52fd04c57e00658
prat33k-dev <bhujbalpratik97@gmail.com>
2021-11-07 13:32:53 -0500

    updated cppcheck and lint shell scripts

--- run_cppcheck.sh
+++ run_cppcheck.sh
@@ -1,3 +1,3 @@
 #!bin/bash
-cppcheck --enable=all --std=c++11 -I src/ --suppress=missingIncludeSystem $( find . -name *.cpp) > results/cppcheck_result.txt
+cppcheck --language=c++ --std=c++11 -I include --suppress=missingIncludeSystem  $( find . -name \*.hpp -or -name \*.cpp) > results/cppcheck_result.txt
 echo "Results are stored in results/cppcheck_result.txt"
